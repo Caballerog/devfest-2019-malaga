4837e4ec00645d1e201ed68ade265cf692a2b970
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2019-12-09 22:41:45 +0100

    feat(middleware): 04-middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,34 @@
-import { CharactersModule } from './CharactersModule/characters.module';
-import { Module } from '@nestjs/common';
+import * as cors from 'cors';
 
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+
+import { CharactersModule } from './CharactersModule/characters.module';
+import { LoggerMiddleware } from './shared/middlewares/logger.middleware';
+
+const routes = {
+  ALL: {
+    path: '*',
+    method: RequestMethod.ALL,
+  },
+  ONLY_GET: {
+    path: '*',
+    method: RequestMethod.GET,
+  },
+};
 @Module({
   imports: [CharactersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(cors())
+      .forRoutes(routes.ALL)
+      .apply(LoggerMiddleware)
+      .forRoutes(routes.ONLY_GET);
+  }
+}
