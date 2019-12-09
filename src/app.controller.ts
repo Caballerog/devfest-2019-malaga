a62df45ebfd0dc93c60d288a7ed89a627ccea1dd
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2019-12-09 20:07:22 +0100

    feat(controllers): 01-controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,70 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
-@Controller()
+import { Character } from './models/character.interface';
+
+@Controller('characters')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private CHARACTERS: Character[] = [];
+  private FAKE_ID = 10000;
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  characters(): Character[] {
+    return this.CHARACTERS;
   }
+
+  @Post()
+  create(@Body('name') name: string) {
+    const character: Character = {
+      name,
+      id: this.FAKE_ID,
+    };
+    this.CHARACTERS = [...this.CHARACTERS, character];
+    this.FAKE_ID = this.FAKE_ID + 1;
+    return character;
+  }
+
+  @Delete(':id')
+  deleteById(@Param('id') id: string): Character {
+    const characterID = parseInt(id, 10);
+    const characterIndex = this.CHARACTERS.findIndex(
+      character => character.id === characterID,
+    );
+    const character = this.CHARACTERS[characterIndex];
+    this.CHARACTERS.splice(characterIndex, 1);
+    return character;
+  }
+
+  @Put()
+  updateById(@Body('id') id: string, 
+             @Body('name') name: string): Character {
+    const characterID = parseInt(id, 10);
+    const character = this.CHARACTERS.find(
+      character => character.id === characterID,
+    );
+    if (character) {
+      character.name = name;
+    }
+    return character;
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Character {
+    const characterID = parseInt(id, 10); // Pipe
+    return this.CHARACTERS.find(({ id }) => id === characterID);
+  }
+
+  @Get('name/:name')
+  findByName(@Param('name') name: string): Character[] {
+    return this.CHARACTERS.filter(character => character.name.includes(name));
+  }
+
+
 }
